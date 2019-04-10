27d284c261954068824a7cd605a4b6225f215ab8
adamjgordon28 <adamjgordon24@gmail.com>
2019-04-10 15:25:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 // Write your code here
-
-class Robot {
-  constructor(name, category, manufactureYear) {
-  this.name = name;
-  this.category = category;
-  this.manufactureYear = manufactureYear
-  }
-}
-
-let robot1 = new Robot('Bender', 'humanoid', '2904')
-let robot2 = new Robot('Wall-E', 'trashCollector', '2093' )
-**************************************************
+//
+// class Robot {
+//   constructor(name, category, manufactureYear) {
+//   this.name = name;
+//   this.category = category;
+//   this.manufactureYear = manufactureYear
+//   }
+// }
+//
+// let robot1 = new Robot('Bender', 'humanoid', '2904')
+// let robot2 = new Robot('Wall-E', 'trashCollector', '2093' )
+// **************************************************
 
 class Breakfast {
-  constructor(food, drink){
+  constructor(food, drink) {
     this.food = food;
     this.drink = drink;
   }
 }
 
 class Lunch {
-  constructor(salad, soup, drink){
+  constructor(salad, soup, drink) {
     this.salad = salad;
     this.soup = soup;
     this.drink = drink;
@@ -28,10 +28,10 @@ class Lunch {
 }
 
 class Dinner {
-  constructor(salad, soup, entree, dessert){
+  constructor(salad, soup, entree, dessert) {
     this.salad = salad;
     this.soup = soup;
     this.entree = entree;
-    this._dessert = dessert;
+    this.dessert = dessert;
   }
 }
