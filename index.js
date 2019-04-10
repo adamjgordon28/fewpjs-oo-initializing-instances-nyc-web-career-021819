05e56b2add78e1070e60798e1705014d4b4243a2
adamjgordon28 <adamjgordon24@gmail.com>
2019-04-10 15:18:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your code here
+
+class Robot {
+  constructor(name, category, manufactureYear) {
+  this.name = name;
+  this.category = category;
+  this.manufactureYear = manufactureYear
+  }
+}
+
+let robot1 = new Robot('Bender', 'humanoid', '2904')
+let robot2 = new Robot('Wall-E', 'trashCollector', '2093' )
+**************************************************
+
+class Breakfast {
+  constructor(food, drink){
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+class Lunch {
+  constructor(salad, soup, drink){
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+
+class Dinner {
+  constructor(salad, soup, entree, dessert){
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this._dessert = dessert;
+  }
+}
